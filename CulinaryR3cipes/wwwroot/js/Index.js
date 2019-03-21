1535401f0e55f55ecb62a6466433e14b875dea51
Hubert Kaszuba <hubert.kaszuba@gmail.com>
2019-03-21 21:43:19 +0100

    Added js file for Index

---
+++ CulinaryR3cipes/wwwroot/js/Index.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    $("#typeDropDown").chosen({
+        width: "33%"
+    });
+});
+
+$(function () {
+    $("#categoryIncludedDropDown").chosen({
+        width: "33%"
+    });
+});
+
+$(function () {
+    $("#categoryExcludedDropDown").chosen();
+    width: "33%"
+});
+
+
+$(document).ready(function () {
+    var selectedTypeIds = $("#typeDropDown").val();
+    $("#viewPlaceHolder").load("/Home/Recipes",
+        { SelectedTypeIds: selectedTypeIds });
+});
+
+$("#typeDropDown").change(function (e) {
+    var selectedTypeIds = $("#typeDropDown").val();
+    $("#viewPlaceHolder").load("/Home/Recipes",
+        { SelectedTypeIds: selectedTypeIds });
+});
+
+$("#btnSearch").on("click", function (e) {
+    var selectedTypeIds = $("#typeDropDown").val();
+    var includedCategoryIds = $("#categoryIncludedDropDown").val();
+    var excludedCategoryIds = $("#categoryExcludedDropDown").val();
+    $("#viewPlaceHolder").load("/Home/Recipes",
+        { SelectedTypeIds: selectedTypeIds, IncludedCategoryIds: includedCategoryIds, ExcludedCategoryIds: excludedCategoryIds, recipePage: 1 });
+});
