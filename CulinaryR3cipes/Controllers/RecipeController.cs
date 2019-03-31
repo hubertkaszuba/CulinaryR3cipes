23aa425b19b6e5f7b3319104f28e8dfd0f1003e9
Hubert Kaszuba <hubert.kaszuba@gmail.com>
2019-03-31 11:15:08 +0200

    Recipe controller

---
+++ CulinaryR3cipes/Controllers/RecipeController.cs
@@ -0,0 +1,30 @@
+﻿using CulinaryR3cipes.Models;
+using CulinaryR3cipes.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CulinaryR3cipes.Controllers
+{
+    public class RecipeController : Controller
+    {
+        IProductRepository productRepository;
+
+        public RecipeController(IProductRepository product)
+        {
+            productRepository = product;
+        }
+
+        public async Task<IActionResult> Recipe()
+        {
+
+            return View(new RecipeViewModel
+            {
+                Recipe = new Models.Recipe(),
+                Products = await productRepository.Products()
+            });
+        }
+    }
+}
---
+++ CulinaryR3cipes/Models/ViewModels/RecipeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CulinaryR3cipes.Models.ViewModels
+{
+    public class RecipeViewModel
+    {
+        public Recipe Recipe { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
