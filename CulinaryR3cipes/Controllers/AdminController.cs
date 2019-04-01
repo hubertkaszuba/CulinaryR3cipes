362b3bf30f413a3a0f09e4cfa4c2d453314fadb3
Hubert Kaszuba <hubert.kaszuba@gmail.com>
2019-04-01 23:03:22 +0200

    Admin panel

---
+++ CulinaryR3cipes/Controllers/AdminController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CulinaryR3cipes.Models.ViewModels;
+using CulinaryR3cipes.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using CulinaryR3cipes.Models;
+
+namespace CulinaryR3cipes.Controllers
+{
+    public class AdminController : Controller
+    {
+        IRecipeRepository recipeRepository;
+        private readonly SignInManager<User> _signInManager;
+
+        public AdminController(IRecipeRepository recipe, SignInManager<User> signInManager)
+        {
+            recipeRepository = recipe;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> RecipesToSubmit()
+        {
+            return PartialView("_RecipesToSubmitListPartial", new RecipesToSubmitListViewModel
+            {
+                RecipesToSubmit = await recipeRepository.FindAllAsync(r => r.IsSubmitted != true)
+            });
+        }
+
+        public IActionResult Users()
+        {
+            return PartialView("_UsersListPartial", new UsersListViewModel
+            {
+                Users = _signInManager.UserManager.Users
+            });
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            return View();
+        }
+    }
+}
